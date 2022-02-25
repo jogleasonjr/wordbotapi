552a82f11cd02a5ab15bf859b53296f5c9a53ec9
John Gleason <jogleasonjr@gmail.com>
2022-02-24 21:18:54 -0600

    Add stringy telegram endpoint

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -18,7 +18,7 @@ public class SearchController : ControllerBase
     }
 
     [HttpGet(Name = "Search")]
-    public async Task<string> Get([FromQuery]string guess, [FromQuery]string? omit = "")
+    public async Task<string> Get([FromQuery] string guess, [FromQuery] string? omit = "")
     {
         if (Sources == null)
         {
@@ -49,29 +49,17 @@ public class SearchController : ControllerBase
     [HttpPost(Name = "Search")]
     public async Task<string> Post(SearchRequest request)
     {
-        if (Sources == null)
-        {
-            Sources = new List<WordSource>();
-            Sources.Add(await WordSource.Create("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt", "English Word List 1"));
-            Sources.Add(await WordSource.Create("https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt", "Google 10,000 Common English Words"));
+        return await Get(request.Guess, request.Omit);
+    }
 
-        }
 
-        var sb = new StringBuilder();
+    [HttpGet("telegram")]
+    public async Task<string> Telegram(string search)
+    {
+        var parts = search.Split(',');
+        var guess = parts[0].Replace('.', ' ');
+        var omit = parts.Length > 1 ? parts[1] : string.Empty;
 
-        foreach (var source in Sources)
-        {
-            sb.AppendLine($"{source.Title} [{source.Words.Length:n0} words]");
-            var results = source.Search(request.Guess, request.Omit);
-
-            for (int i = 0; i < results.Length; i++)
-            {
-                sb.AppendLine($"{(i + 1).ToString().PadLeft(2)}. {results[i]}");
-            }
-
-            sb.AppendLine();
-        }
-
-        return sb.ToString();
+        return await Get(guess, omit);
     }
 }
